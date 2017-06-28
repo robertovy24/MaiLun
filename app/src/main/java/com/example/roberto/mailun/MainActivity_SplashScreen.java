cd44316a67681f29bc9879b4d0f1388f497d026c
Roberto Villalobos <roberto@MacBook-Pro-de-Roberto.local>
2017-06-27 22:48:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/roberto/mailun/MainActivity_SplashScreen.java
@@ -0,0 +1,13 @@
+package com.example.roberto.mailun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__splash_screen);
+    }
+}
